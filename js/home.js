e386741b276582b2c9c1aa4a75bef36eccd4454b
developersujoy <sujaymaity818@gmail.com>
2026-03-07 19:13:38 +0530

    loading spinner

--- js/home.js
+++ js/home.js
@@ -1,23 +1,70 @@
 console.log("connected sucessfully")
 
 const catagoriesContainer = document.getElementById("CatagoriesSection");
-
-
+const loadingSpinner = document.getElementById("loadingSpinner");
+let allIssues = [];
+const allBtn = document.getElementById("allBtn");
+const closedBtn = document.getElementById("closedBtn");
+const openBtn = document.getElementById("openBtn");
 
 
 async function LoadCatagories(){
+loadingSpinner.classList.remove("hidden");
+loadingSpinner.classList.add("flex");
+    // dom load korar jonno time dea holo nicher line ea jate loading spinner dekha jaay
+    await new Promise(resolve => setTimeout(resolve, 300));
+    //fentch data
     const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await response.json();
+    loadingSpinner.classList.add("hidden");
     displaydata(data.data);
-    console.log(data)
+    //all data store
+    allIssues=data.data;
+    displaydata(allIssues)
+    console.log(data);
+
 }
+
 LoadCatagories()
 const itemlist = document.getElementById("itemlists");
+itemlist.innerHTML = " " // old card will clear when reload
 function displaydata(allissues) {
     console.log(allissues);
     allissues.forEach(issue => {
+        // formated date
+        const date = new Date(issue.createdAt);
+        const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        // create element 
     const card = document.createElement("div");
-        // card.innerHTML =`<p>Hello worlds</p>` ;
+        card.innerHTML =`
+        <div class="card bg-base-100 w-full h-full shadow-sm">
+                <div class="p-4">
+                    <div class="flex justify-between ">
+                        <div>
+                            <img src=" ${issue.priority === "high"
+            ? "./assets/Open-Status.png" : issue.priority == "medium" ? "./assets/Open-Status.png" : "./assets/Closed- Status .png "} " alt="" srcset="">
+                        </div>
+                
+                            <div class="p-2 w-20 items-center justify-center h-6 rounded-full flex ${issue.priority == "high" ? "bg-[#FEECEC] text-[#EF4444]" : issue.priority == "medium" ? "bg-[#FFF6D1] text-[#F59E0B]" : "bg-[#EEEFF2] text-[#9CA3AF]"}">
+                                <p class="text-[12px] font-medium"> ${issue.priority} </p>
+                            </div>
+                    </div>
+                    <div class="mt-3 space-y-2">
+                        <h3 class="font-semibold text-[14px] h-[36px]"> ${issue.title} </h3>
+                        <p class="text-[12px] text-[#64748B] line-clamp-2 "> ${issue.description} </p>
+                    </div>
+                    <div class="flex mt-3 gap-1">
+                        <div><img src="./assets/Labels.png" alt="" srcset=""></div>
+                        <div><img src="./assets/Labels (1).png" alt="" srcset=""></div>
+                    </div>
+                </div>
+                <hr class=" border-gray-300">
+                <div class="p-4 text-[12px] text-[#64748B] space-y-2">
+                    <p> # ${issue.id} by ${issue.author}</p>
+                    <p>${formattedDate}</p>
+                </div>
+                
+            </div>` ;
         itemlist.appendChild(card);
     });
 }
